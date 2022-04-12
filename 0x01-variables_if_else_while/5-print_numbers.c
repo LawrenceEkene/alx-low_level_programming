725aa2cf189ec47a8c57854ed512cf9c97b8406a
LawrenceEkene <ekenelawrence247@gmail.com>
2022-04-12 16:25:02 -0700

    print single base numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Printing signle base numbers
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+int i;
+
+int ASCII = 10;
+
+for (i = 0; i < 10; i++)
+
+{
+
+printf("%d", i);
+
+}
+
+printf("%c", ASCII);
+
+return (0);
+
+}
