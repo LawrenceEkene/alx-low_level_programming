11860bedc1767d2b601772984e4f781d225dc6ec
LawrenceEkene <ekenelawrence247@gmail.com>
2022-04-13 00:15:38 -0700

    check if value is lower or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * _islower - single line and c have value compared
+
+ */
+
+int _islower(char c)
+
+{
+
+if(c < 'a' && c < 'z')
+
+{
+
+return (1);
+
+}
+
+else
+
+{
+
+return (0);
+
+}
+
+}
